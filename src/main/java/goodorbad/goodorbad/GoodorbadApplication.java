069c24edfefbb7adf33d2bcc2ed1807f1ad486df
HaDaHyeon <hada3817@gmail.com>
2024-07-24 14:10:49 +0900

    first

---
+++ src/main/java/goodorbad/goodorbad/GoodorbadApplication.java
@@ -0,0 +1,13 @@
+package goodorbad.goodorbad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoodorbadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoodorbadApplication.class, args);
+	}
+
+}
